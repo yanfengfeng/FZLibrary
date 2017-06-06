9250d9dfc8b244cd7914a3995435435b35c3f9b2
yanfengfeng <yanff2000@126.com>
2017-06-06 10:12:53 +0800

    测试

---
+++ MyApplication/fzlibrary/src/main/java/com/kysoft/fzlibrary/Text.java
@@ -0,0 +1,12 @@
+package com.kysoft.fzlibrary;
+
+/**
+ * 作者: {yff} time: 2017/6/6  10:09
+ * 版本 viersionCode:{1} viersionName:{V1.0.0}
+ * 备注{}
+ */
+
+public class Text {
+
+    public  static String dd = "11";
+}
